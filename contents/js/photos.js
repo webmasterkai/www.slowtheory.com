138ffdaa35b6e5363908782fcaae81333bebcf9a
Tim Lupfer <tim@sundaysenergy.com>
2013-12-02 20:13:55 -0600

    New photo processing

--- contents/js/photos.js
+++ contents/js/photos.js
@@ -2,19 +2,21 @@
 function load_photos(current_page, template) {
   // Show the overlay and disable
   $('.overlay').show();
-  $('.loadmore .nextgroup').off('click');
+  if (window.location.hash) { current_page = parseInt(window.location.hash.replace("#", '')); }
+
   var next_page = parseInt(current_page) + 1;
-  var params = { 'page' : next_page, 'pageSize' : 60, 'returnSizes' : '300x300xCR,1024x1024' };
-  if (window.location.hash) { params.tags = window.location.hash.replace("#!/", ''); }
+  var previous_page = parseInt(current_page) - 1;
+  var params = { 'page' : current_page, 'pageSize' : 60, 'returnSizes' : '300x300xCR,1024x1024' };
 
   // Get a list of files from trovebox
   $.getJSON('http://www.slowtheory.com/list', params, function(data) {
-    if (data.result.length == 0) { $('.loadmore .nextgroup').addClass('disabled'); }
     var html = template(data);
     $.when($('#photos .list').html($(html))).then(function() {
+      // Lazyload the thumbnails
       $("img.lazy").show().lazyload({
         skip_invisible : false
       });
+      // Add fancyboxes to the thumbs
       $("#photos .list .fancybox").fancybox({
           helpers : {
             title : {
@@ -24,22 +26,42 @@ function load_photos(current_page, template) {
          openEffect  : 'none',
          closeEffect : 'none'
       });
-      $('html,body').trigger("scroll");  
+      // Simulate scrolling to trigger lazyload
+      $('html,body').trigger("scroll");
       $(window).resize(function() {
         $("img.lazy").show().lazyload();
       });
     });
+    // Hide the overlay after we've loaded in the new images
     $('.overlay').hide();
+    // Scroll to the top of the page
     $('html, body').animate({ scrollTop: $('body').offset().top }, 0);
+    // If we have more total rows than what is shown, enable the button
+    if (data.result[0].totalRows > (60 * next_page)) {
+      $('.nextpage').removeClass('disabled');
+      $('.nextpage').find('a').attr('href', '/photos/#' + next_page);
+    } else {
+      $('.nextpage').addClass('disabled');
+    }
+    // If we aren't on the first page, allow us to go back
+    if (current_page > 1) {
+      $('.prevpage').removeClass('disabled');
+      $('.prevpage').find('a').attr('href', '/photos/#' + previous_page);    
+    } else {
+      $('.prevpage').addClass('disabled');
+    }
   });
   return true;
 }
 
 $('document').ready(function() {
+  // Compile the template for list items
   var template = Handlebars.compile('{{#each result}}<li class="col-sm-3 col-xs-6"><a class="fancybox" rel="gallery" href="{{path1024x1024}}"><img class="img-responsive lazy" src="/assets/media/black.png" data-original="{{path300x300xCR}}" alt="{{title}}" title="{{description}}"></a></li>{{/each}}');
-  load_photos(0,template);
-  // $(window).on('hashchange', function() {
-  //   $('html, body').animate({ scrollTop: $('body').offset().top }, 500);
-  //   load_photos(0,template);
-  // });
+  // Load the initial set of photos
+  load_photos(1,template);
+  // On hash change, load the new group of photos
+  $(window).on('hashchange', function() {
+    load_photos(window.location.hash.replace("#", ''),template);
+    $('html, body').animate({ scrollTop: $('body').offset().top }, 500);
+  });
 });
