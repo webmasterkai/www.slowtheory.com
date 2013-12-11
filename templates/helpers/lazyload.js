a71cd4a645d16b4aa257dc0b874757a1469eb980
Tim Lupfer <tim@sundaysenergy.com>
2013-12-10 20:33:19 -0600

    Move font assets to theme repo

--- templates/helpers/lazyload.js
+++ templates/helpers/lazyload.js
@@ -1,4 +1,4 @@
 module.exports = function(html) {
   var re = /img src="([^"]+)"([^>]*>)/g;
-  return html.replace(re, "img src=\"/assets/media/grey.gif\" class=\"lazy img-responsive\" data-original=\"$1\"$2<noscript><img src=\"$1\" alt=\"Enable javascript for faster image loading.\"></noscript>");
+  return html.replace(re, "img src=\"/assets/media/black.png\" class=\"lazy img-responsive\" data-original=\"$1\"$2<noscript><img src=\"$1\" alt=\"Enable javascript for faster image loading.\"></noscript>");
 }
