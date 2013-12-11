1dcb65e51e024532e7a0c0ec4840c7aa7138a788
Tim Lupfer <tim@sundaysenergy.com>
2013-12-10 20:54:22 -0600

    transparent png

--- templates/helpers/imagify.js
+++ templates/helpers/imagify.js
@@ -2,7 +2,7 @@ module.exports = function(html) {
   var re = new RegExp("img src=\"([^\"]+)\"([^>]*>)",'g');
   if (html.match(re, 'g') != null) {
     new RegExp("src=\"([^\"]+)","g");
-    return html.match(re, 'g')[0].replace(re, "<img src=\"/assets/media/black.png\" class=\"lazy img-responsive\" data-original=\"$1\"$2<noscript><img src=\"$1\" alt=\"Enable javascript for faster image loading.\"></noscript>").replace(/http:\/\/([^\/]+)[\/]([^"]+)/, "http://$1.img.labori.us/c350x250/$2");
+    return html.match(re, 'g')[0].replace(re, "<img src=\"/assets/media/t.png\" class=\"lazy img-responsive\" data-original=\"$1\"$2<noscript><img src=\"$1\" alt=\"Enable javascript for faster image loading.\"></noscript>").replace(/http:\/\/([^\/]+)[\/]([^"]+)/, "http://$1.img.labori.us/c350x250/$2");
   }
   return '<div class="no-photo"><i class="fa fa-pencil-square-o fa-5x"></i></div>';
 }
